768f02e944f2db1ad771e2c571a5cdcd1c9fdeb3
Lucasfi <lukafigueroa64@gmail.com>
2025-07-02 21:15:44 -0300

    agregamos la funcion logout

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,39 @@ import FormularioProducto from "./components/pages/producto/FormularioProducto";
 import Footer from "./components/shared/Footer";
 import Menu from "./components/shared/Menu";
 import Login from "./components/pages/Login";
+import { useState } from "react";
 
 function App() {
+  const usuarioLogueado =
+    JSON.parse(sessionStorage.getItem("userKey")) || false; // Se puede guardar con True o false
+  const [usuarioAdmin, setUsuarioAdmin] = useState(usuarioLogueado);
+
   return (
     <>
       <BrowserRouter>
-        <Menu></Menu>
+        <Menu usuarioAdmin={usuarioAdmin} setUsuarioAdmin={setUsuarioAdmin}></Menu>
         <main>
           <Routes>
             <Route path="/" element={<Inicio></Inicio>}></Route>
             <Route path="/detalle" element={<DetalleProducto />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/administrador/" element={<FormularioProducto />}></Route>
-            <Route path="/administrador/editar" element={<FormularioProducto />}></Route>
+            <Route
+              path="/administrador/"
+              element={<DetalleProducto />}
+            ></Route>
+            <Route
+              path="/login"
+              element={<Login setUsuarioAdmin={setUsuarioAdmin} />}
+            ></Route>
+            <Route
+              path="/administrador/crear"
+              element={<Administrador></Administrador>}
+            ></Route>
+            <Route
+              path="/administrador/editar"
+              element={<FormularioProducto></FormularioProducto>}
+            ></Route>
             <Route path="*" element={<Error404 />}></Route>
           </Routes>
-          <FormularioProducto></FormularioProducto>
         </main>
         <Footer></Footer>
       </BrowserRouter>
--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -1,12 +1,20 @@
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import logo from "../../assets/coffee_logo.png";
-import { NavLink, Link } from "react-router";
+import { NavLink, Link, useNavigate } from "react-router";
 
-const Menu = () => {
+const Menu = ({ usuarioAdmin, setUsuarioAdmin }) => {
+  const navegacion = useNavigate()
+
+  const logout = () => {
+    setUsuarioAdmin(false)
+    sessionStorage.removeItem('userKey')
+    navegacion('/')
+  }
+  
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand as={Link} to={'/'}>
+        <Navbar.Brand as={Link} to={"/"}>
           <img
             src={logo}
             alt="logo Rolling Coffee"
@@ -17,11 +25,21 @@ const Menu = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <NavLink className='nav-link' to={'/'}>Inicio</NavLink>
-            <NavLink className="nav-link" to={'/administrador'}>Administrador</NavLink>
-            <Button className="nav-link " variant="link" >
-              Login
-            </Button>
+            <NavLink className="nav-link" to={"/"}>
+              Inicio
+            </NavLink>
+            {usuarioAdmin ? (
+              <>
+                <NavLink className="nav-link" to={"/administrador"}>
+                  Administrador
+                </NavLink>
+                <Button className="nav-link" onClick={logout}>Logout</Button>
+              </>
+            ) : (
+              <NavLink className="nav-link" to={"/login"}>
+                Login
+              </NavLink>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
